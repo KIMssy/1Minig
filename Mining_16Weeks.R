6e3c7cfc753a476af887fafc7e3752122fc4731e
KIMssy <112833993+KIMssy@users.noreply.github.com>
2022-12-24 23:47:30 +0900

    Add files via upload

---
+++ Mining_16Weeks.R
@@ -0,0 +1,22 @@
+attach(trees)
+str(trees)
+head(trees)
+install.packages("psych")
+library(psych)
+describe(trees)
+3.14/sqrt(31)
+0.553/0.421
+-0.394/0.421
+1.12/0.421
+1-0.9082
+0.09182
+qqnorm(Girth)
+qqline(Girt, col-"blue")
+shapiro.test(Girth)
+-0.71/0.821
+1-0.805
+0.195*2
+1.96*0.56
+13.25-1.10
+
+str(trees)*2
